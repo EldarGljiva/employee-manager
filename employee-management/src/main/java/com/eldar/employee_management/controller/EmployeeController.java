e585559085a5342c194010a5e0b1f9db15e43ff5
Eldar Gljiva <=eldar.gljiva@stu.ibu.edu.ba>
2024-09-22 14:18:29 +0200

    finish simple project

--- employee-management/src/main/java/com/eldar/employee_management/controller/EmployeeController.java
+++ employee-management/src/main/java/com/eldar/employee_management/controller/EmployeeController.java
@@ -2,15 +2,18 @@ package com.eldar.employee_management.controller;
 
 import com.eldar.employee_management.entity.Employee;
 import com.eldar.employee_management.service.EmployeeService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -18,6 +21,41 @@ public class EmployeeController {
     @PostMapping("/employee")
     public Employee postEmployee(@RequestBody Employee employee) {
         return employeeService.postEmployee(employee);
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable Long id){
+        try{
+            employeeService.deleteEmployee(id);
+            return new ResponseEntity<>("Employee with ID " +id +" deleted succesfully", HttpStatus.OK);
+        }
+        catch(EntityNotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<?> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeService.getEmployeeById(id);
+        if(employee == null){
+            return ResponseEntity.notFound().build();
+
+        }
+        return ResponseEntity.ok(employee);
+    }
+
+    @PatchMapping("/employee/{id}")
+    public ResponseEntity<?> updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        Employee updatedEmployee = employeeService.updateEmployee(id, employee);
+        if(updatedEmployee == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(updatedEmployee);
     }
 
 }
--- employee-management/src/main/java/com/eldar/employee_management/service/EmployeeService.java
+++ employee-management/src/main/java/com/eldar/employee_management/service/EmployeeService.java
@@ -2,8 +2,12 @@ package com.eldar.employee_management.service;
 
 import com.eldar.employee_management.Repository.EmployeeRepository;
 import com.eldar.employee_management.entity.Employee;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +17,35 @@ public class EmployeeService {
 
     public Employee postEmployee(Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public void deleteEmployee(Long id){
+        if(!employeeRepository.existsById(id)){
+            throw new EntityNotFoundException("Employee with ID " + id + " not found");
+        }
+        employeeRepository.deleteById(id);
+    }
+
+    public Employee getEmployeeById(Long id){
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public Employee updateEmployee(Long id, Employee employee){
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        if(optionalEmployee.isPresent()){
+            Employee existingEmployee = optionalEmployee.get();
+
+            existingEmployee.setName(employee.getName());
+            existingEmployee.setDepartment(employee.getDepartment());
+            existingEmployee.setEmail(employee.getEmail());
+            existingEmployee.setPhone(employee.getPhone());
+
+            return employeeRepository.save(existingEmployee);
+        }
+        return null;
     }
 }
